5810463a7623cf82b7cf51bf86d8e2af8c045add
Jason Frame <jason@onehackoranother.com>
2014-02-09 19:34:17 +0000

    tabs -> spaces

--- test.js
+++ test.js
@@ -9,44 +9,44 @@ var T = require('./lib/ast/types');
 var D = require('./lib/dispatch');
 
 env.define(root, 'abs', D(
-	['value'], function(env, val) {
-		return ast.value(Math.abs(val.number), val.unit);
-	},
-	['number'], function(env, val) {
-		return Math.abs(val);
-	}
+    ['value'], function(env, val) {
+        return ast.value(Math.abs(val.number), val.unit);
+    },
+    ['number'], function(env, val) {
+        return Math.abs(val);
+    }
 ));
 
 var p1 = ast.pair('margin', [
-	ast.value(10, 'px'),
-	ast.value(20, 'px'),
-	ast.value(30, 'px'),
-	ast.value(40, 'px')
+    ast.value(10, 'px'),
+    ast.value(20, 'px'),
+    ast.value(30, 'px'),
+    ast.value(40, 'px')
 ]);
 
 var p2 = ast.pair('padding', [
-	ast.call('abs', [
-		ast.binOp('*',
-			-1.5,
-			ast.binOp('+',
-				ast.value(10, 'px'),
-				ast.value(30, 'px')))
-	]),
-	ast.value(10, 'px'),
-	ast.value(20, 'px'),
-	ast.value(30, 'px')
+    ast.call('abs', [
+        ast.binOp('*',
+            -1.5,
+            ast.binOp('+',
+                ast.value(10, 'px'),
+                ast.value(30, 'px')))
+    ]),
+    ast.value(10, 'px'),
+    ast.value(20, 'px'),
+    ast.value(30, 'px')
 ]);
 
 var p3 = ast.pair('background-color', ['#ff0000']);
 
 var r1 = ast.rule('&.foo', [
-	p1,
-	p2
+    p1,
+    p2
 ]);
 
 var r2 = ast.rule('body', [
-	r1,
-	p3
+    r1,
+    p3
 ]);
 
 var rules = ast.rules([r2]);
